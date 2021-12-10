ce0c392d16b86be054b34c7b1959f6e08bc284b1
Grace Hephzibah M <63595093+Grace-Hephzibah@users.noreply.github.com>
2021-12-10 13:53:17 +0530

    Update Data_Transmission_1.c

--- Data_Transmission_1.c
+++ Data_Transmission_1.c
@@ -1,3 +1,28 @@
+/*
+Data Transmission - I
+The company has transmitted its data to the target server successfully. Now, on the new server, the transmitted binary data has to be stored in a user-readable format. As a developer, you have been assigned the task of converting the received binary data to user-readable decimal data.
+
+Write an algorithm to convert the given data to the decimal format.
+
+Input:
+
+The input consists of an integer data, representing the binary form of the data received.
+
+Output:
+
+Print an Integer representing the decimal form of the received data.
+
+Example
+
+Input:
+
+1001111001
+
+Explanation:
+
+On converting the given binary data, the output is 633.
+*/
+
 #include <stdio.h>
 int main()
 {
